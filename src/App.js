f9f0ed9ecee76a97cfa2280cc9de3ff263a24b79
Caio Cabral <ocaiocabral@gmail.com>
2023-02-02 13:52:29 -0400

    feat: created routes and MUI

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import DrawerAppBar from './Componentes/Menu/DrawerAppBar';
+import Inicio from './pages/Inicio/Inicio'
+import SobreMim from './pages/SobreMim/SobreMim'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Olá, mundo</h1>
-      </header>
-    </div>
+    <BrowserRouter>
+      <DrawerAppBar></DrawerAppBar>
+      <Routes>
+        <Route
+          path="/Home"
+          element={<Inicio />}
+         />
+        <Route
+          path="/About"
+          element={<Inicio />}
+         />
+         <Route
+          path="/Contact"
+          element={<SobreMim />}
+         />
+         <Route
+          path="*"
+          element={<h1>Página não encontrada</h1>}
+         />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/SobreMim/SobreMim.jsx
@@ -0,0 +1,7 @@
+function SobreMim() {
+   return(
+      <h1>SobreMim</h1>
+   )
+}
+
+export default SobreMim;
